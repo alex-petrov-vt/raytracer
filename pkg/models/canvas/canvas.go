85093f889269eadbd0b1251ee78de752dd0f5257
Alex Petrov <alex.petrov.vt@gmail.com>
2021-04-25 20:14:41 -0400

    Refactor canvas a bit

--- pkg/models/canvas/canvas.go
+++ pkg/models/canvas/canvas.go
@@ -10,6 +10,12 @@ import (
 	"github.com/alex-petrov-vt/raytracer/pkg/models/color"
 )
 
+const (
+	ppmMagicNumber = "P3"
+	colorRange     = 255
+	maxLineSize    = 70
+)
+
 // Canvas is a representation of a screen of dimensions widht x height where all
 // of pixels' colors are stored in internal 2D Colors slice.
 type Canvas struct {
@@ -20,19 +26,23 @@ type Canvas struct {
 // NewCanvas creates new canvas of dimensions w x h (widht x height) and initializes
 // all pixels to black color.
 func NewCanvas(w, h int) *Canvas {
-	colors := make([][]*color.Color, h)
-	for j := range colors {
-		colors[j] = make([]*color.Color, w)
-		for i := range colors[j] {
-			colors[j][i] = color.NewColor(0, 0, 0)
-		}
-	}
-
+	colors := newColorMap(w, h)
 	return &Canvas{
 		Height: h,
 		Width:  w,
 		Colors: colors,
 	}
+}
+
+func newColorMap(w, h int) [][]*color.Color {
+	colors := make([][]*color.Color, h)
+	for row := range colors {
+		colors[row] = make([]*color.Color, w)
+		for rowItem := range colors[row] {
+			colors[row][rowItem] = color.NewColor(0, 0, 0)
+		}
+	}
+	return colors
 }
 
 // WritePixel writes a color provided in col to the pixel located at [w][h]
@@ -45,69 +55,67 @@ func (c *Canvas) GetPixel(w, h int) *color.Color {
 	return c.Colors[h][w]
 }
 
+// SaveToPPM saves canvas to a .ppm file
 func (c *Canvas) SaveToPPM(file string) error {
 	handle, err := os.Open(file)
 	if err != nil {
 		return err
 	}
 	defer handle.Close()
-
-	err = writePPM(handle, c)
-	return err
+	return writePPM(handle, c)
 }
 
 func writePPM(handle io.Writer, c *Canvas) error {
 	w := bufio.NewWriter(handle)
-
-	err := writeHeader(w, c)
-	if err != nil {
+	if err := writeHeader(w, c); err != nil {
 		return err
 	}
-
-	err = writeData(w, c)
-	return err
+	return writeData(w, c)
 }
 
 func writeHeader(w *bufio.Writer, c *Canvas) error {
-	header := fmt.Sprintf("P3\n%d %d\n255\n", c.Width, c.Height)
+	header := fmt.Sprintf("%s\n%d %d\n%d\n", ppmMagicNumber, c.Width, c.Height, colorRange)
 	_, err := w.Write([]byte(header))
 	if err != nil {
 		return err
 	}
-	err = w.Flush()
-	return err
+	return w.Flush()
 }
 
 func writeData(w *bufio.Writer, c *Canvas) error {
 	for _, row := range c.Colors {
 		rowString := ""
-		lineLenght := 0
-		for rowCount, c := range row {
-			colorToSave := color.ColorTo255Range(c)
+		lineLength := 0
+		for colorCount, currColor := range row {
+			colorToSave := color.ColorTo255Range(currColor)
 			colorToSaveString := fmt.Sprintf("%d %d %d", int(colorToSave.Red), int(colorToSave.Green), int(colorToSave.Blue))
 			rowString += colorToSaveString
-			lineLenght += utf8.RuneCountInString(colorToSaveString)
-			if lineLenght > 70 {
-				for i := utf8.RuneCountInString(rowString) - (lineLenght - 70); i >= 0; i-- {
-					if rowString[i] == ' ' {
-						rowString = rowString[:i] + "\n" + rowString[i+1:]
-						lineLenght = len(rowString[i+1:])
-						break
-					}
-				}
+			lineLength += utf8.RuneCountInString(colorToSaveString)
+			if lineLength > maxLineSize {
+				rowString, lineLength = splitLongLine(rowString, lineLength)
 			}
-			if rowCount != len(row)-1 {
+			if colorCount != len(row)-1 {
 				rowString += " "
-				lineLenght += 1
+				lineLength += 1
 			} else {
 				rowString += "\n"
 			}
 		}
-		_, err := w.Write([]byte(rowString))
-		if err != nil {
+
+		if _, err := w.Write([]byte(rowString)); err != nil {
 			return err
 		}
 	}
-	err := w.Flush()
-	return err
+	return w.Flush()
+}
+
+func splitLongLine(line string, lineLength int) (string, int) {
+	for i := utf8.RuneCountInString(line) - (lineLength - maxLineSize); i >= 0; i-- {
+		if line[i] == ' ' {
+			line = line[:i] + "\n" + line[i+1:]
+			lineLength = len(line[i+1:])
+			break
+		}
+	}
+	return line, lineLength
 }
