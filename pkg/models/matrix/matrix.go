985b0e2db1d8f7ba502aa4b5b88873967feba30d
Alex Petrov <alex.petrov.vt@gmail.com>
2021-04-29 20:13:07 -0400

    Refactor matrix to have width and height properties

--- pkg/models/matrix/matrix.go
+++ pkg/models/matrix/matrix.go
@@ -3,12 +3,22 @@ package matrix
 import "errors"
 
 type Matrix struct {
-	elements [][]float64
+	Width, Height int
+	Elements      [][]float64
 }
 
 // NewMatrix creates new matrix given an input 2D array
 func NewMatrix(input [][]float64) *Matrix {
+	if len(input) == 0 {
+		return &Matrix{
+			len(input),
+			0,
+			input,
+		}
+	}
 	return &Matrix{
+		len(input),
+		len(input[0]),
 		input,
 	}
 }
@@ -16,24 +26,21 @@ func NewMatrix(input [][]float64) *Matrix {
 // GetElement returns an element with coordinates [row][col] or error if those
 // are outside of the boundaries of the matrix
 func (m *Matrix) GetElement(row, col int) (float64, error) {
-	if row < 0 || row >= len(m.elements) || col < 0 || col >= len(m.elements[0]) {
+	if row < 0 || row >= m.Height || col < 0 || col >= m.Width {
 		return 0, errors.New("access elements outside of matrix")
 	}
-	return m.elements[row][col], nil
+	return m.Elements[row][col], nil
 }
 
 // Equals compares two matrices for equality
 func Equals(m1, m2 *Matrix) bool {
-	if len(m1.elements) != len(m2.elements) {
-		return false
-	}
-	if len(m1.elements) > 0 && len(m2.elements) > 0 && len(m1.elements[0]) != len(m2.elements[0]) {
+	if m1.Height != m2.Height || m1.Width != m2.Width {
 		return false
 	}
 
-	for row := range m1.elements {
-		for col := range m1.elements[row] {
-			if m1.elements[row][col] != m2.elements[row][col] {
+	for row := range m1.Elements {
+		for col := range m1.Elements[row] {
+			if m1.Elements[row][col] != m2.Elements[row][col] {
 				return false
 			}
 		}
@@ -43,20 +50,20 @@ func Equals(m1, m2 *Matrix) bool {
 
 //Multiply multiplies two matrices together
 func Multiply(m1, m2 *Matrix) (*Matrix, error) {
-	if len(m1.elements) == 0 || len(m2.elements) == 0 || len(m1.elements[0]) == 0 || len(m2.elements[0]) == 0 {
+	if m1.Height == 0 || m1.Width == 0 || m2.Height == 0 || m2.Width == 0 {
 		return nil, errors.New("one or both of matrices are empty")
 	}
-	if len(m1.elements) != len(m2.elements[0]) {
+	if m1.Width != m2.Height {
 		return nil, errors.New("incompatible dimensions for matrix multiplication")
 	}
 
 	var newElems [][]float64
-	for row := range m1.elements {
+	for row := range m1.Elements {
 		var newRow []float64
-		for col := range m1.elements[row] {
+		for col := range m1.Elements[row] {
 			result := 0.0
-			for i := range m2.elements {
-				result += m1.elements[row][i] * m2.elements[i][col]
+			for i := range m2.Elements {
+				result += m1.Elements[row][i] * m2.Elements[i][col]
 			}
 			newRow = append(newRow, result)
 		}
